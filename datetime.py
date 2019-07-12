cbceb99b9e68563cde6ef9cb7ff926f6cb059372
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-12 13:52:48 +0100

    Create datetime.py

---
+++ datetime.py
@@ -0,0 +1,12 @@
+from datetime import datetime 
+
+current_time = datetime.now()
+
+print(current_time)
+
+
+'''
+This is a way to import the current date and time without all the code
+Line 1 imports the module 
+line 3 sets the variable for datetime.now() - the current time
+'''
