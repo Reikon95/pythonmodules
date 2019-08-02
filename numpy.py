fa91df38679408967ec6769a92b4f8783a6f01dd
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-02 10:37:10 +0100

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -54,3 +54,14 @@ third_quarter = np.percentile(movies_watched, 75)
 interquartile_range  = (third_quarter - first_quarter)
 
 
+# standard deviation
+
+pumpkin = np.array([68, 1820, 1420, 2062, 704, 1156, 1857, 1755, 2092, 1384])
+
+acorn_squash = np.array([20, 43, 99, 200, 12, 250, 58, 120, 230, 215])
+
+pumpkin_std = np.std(pumpkin)
+
+acorn_squash_std = np.std(acorn_squash)
+
+winner = pumpkin
